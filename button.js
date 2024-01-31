a06089fa459a4d7c3808a1b5e2431ce228fd4eeb
QuanyshK <zoomer.qurbany@gmail.com>
2024-02-01 01:44:40 +0600

    update

--- button.js
+++ button.js
@@ -1,4 +1,3 @@
-const question = document.querySelector('.question');
 const wrapper = document.querySelector('.wrapper');
 const yesBtn = document.querySelector('.yes-btn');
 const noBtn = document.querySelector('.no-btn');
