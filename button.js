e3e2d7292a1199c7c0d08d7fcff1e0ff96988cb0
QuanyshK <zoomer.qurbany@gmail.com>
2024-02-01 00:47:08 +0600

    first

---
+++ button.js
@@ -0,0 +1,15 @@
+const question = document.querySelector('.question');
+const wrapper = document.querySelector('.wrapper');
+const yesBtn = document.querySelector('.yes-btn');
+const noBtn = document.querySelector('.no-btn');
+yesBtn.addEventListener('click', function() {
+    window.location.href = 'happy.html';
+});
+noBtn.addEventListener('mouseover', () => {
+    const wrapperRect = wrapper.getBoundingClientRect();
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
+    const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
+        noBtn.style.left = i + 'px';
+        noBtn.style.top = j + 'px';
+});
